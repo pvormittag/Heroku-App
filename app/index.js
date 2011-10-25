279c9119de10b468345a9d14a78112c1f761964b
Patrik Vormittag <patrik.vormittag@gmail.com>
2011-10-25 16:40:28 -0600

    testing mongodb support

--- app/index.js
+++ app/index.js
@@ -4,6 +4,8 @@ var express = require('express'),
 	
 app.use(express.static(__dirname + '/../public'));
 
+console.log('mongodb:', process.env.MONGOHQ_URL);
+
 app.listen(port, function(){
 	console.log('application running on ' + port);
 });
